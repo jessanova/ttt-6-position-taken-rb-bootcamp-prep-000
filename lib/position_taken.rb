5acdc9ead6a98034fe43bf60eb662e2b5ae84173
Jess Jacobson <jessanova@gmail.com>
2018-10-16 18:48:49 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,30 @@
 # code your #position_taken? method here!
+
+def position_taken?(board,index)
+  #if board[index]==(" "||""||nil)
+  if (board[index]==" ") || (board[index].empty?) || (board[index].nil?)
+    return false
+  end
+  return true
+end
+
+
+def tester
+  if " "==" " 
+    puts "yes1"
+  end
+  if ""==""
+    puts "yes2"
+  end
+  if "".empty?
+    puts "yes3"
+  end
+  if nil==nil
+    puts "yes4"
+  end
+  if nil.nil?
+    puts "yes5"
+  end
+end
+
+tester
